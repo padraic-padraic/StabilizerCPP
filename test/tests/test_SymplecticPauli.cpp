3feb3f84dd245e26abb4264f57e80e8d506af1a9
Padraic Calpin <padraic.calpin93@gmail.com>
2017-04-10 14:19:05 +0100

    Finish Unary Symplectic Pauli Tests

--- test/tests/test_SymplecticPauli.cpp
+++ test/tests/test_SymplecticPauli.cpp
@@ -3,8 +3,10 @@
 //
 
 #include "gtest/gtest.h"
+#include "lib/SymplecticPauli.h"
+#include <string>
 
-class SPConstructTests : public ::testing::Test {
+class SPUnaryTests : public ::testing::Test {
 protected:
     unsigned int NQUBITS=4;
     unsigned int XNUM = 6;
@@ -12,5 +14,34 @@ protected:
     unsigned int NUM = 101; // == XNUM*(2^4) + ZNUM;
 };
 
-TEST_F(SPConstructTests, DefaultConstructor){
+TEST_F(SPUnaryTests, DefaultConstructor){
+    SymplecticPauli p;
+    EXPECT_EQ(p.NQubits(), 0);
+    EXPECT_EQ(p.XBits().size(), 0);
+    EXPECT_EQ(p.ZBits().size(), 0);
+}
+
+TEST_F(SPUnaryTests, LengthConstructor){
+    SymplecticPauli p(NQUBITS);
+    EXPECT_EQ(p.NQubits(), NQUBITS);
+    EXPECT_EQ(p.XBits().size(),NQUBITS);
+    EXPECT_EQ(p.ZBits().size(),NQUBITS);
+}
+
+TEST_F(SPUnaryTests, TwoNumberConstructor){
+    SymplecticPauli p(NQUBITS, XNUM, ZNUM);
+    EXPECT_EQ(p.XBits().to_ulong(), XNUM);
+    EXPECT_EQ(p.ZBits().to_ulong(), ZNUM);
+}
+
+TEST_F(SPUnaryTests, OneNumberConstructor){
+    SymplecticPauli p(NQUBITS, NUM);
+    EXPECT_EQ(p.XBits().to_ulong(), XNUM);
+    EXPECT_EQ(p.ZBits().to_ulong(), ZNUM);
+}
+
+TEST_F(SPUnaryTests, HashCheck){
+    SymplecticPauli p(NQUBITS, NUM);
+    std::string expected = std::to_string(NQUBITS) + std::to_string(XNUM) + std::to_string(ZNUM);
+    ASSERT_EQ(std::hash<std::string>{}(expected), PauliHash{}(p));
 }
