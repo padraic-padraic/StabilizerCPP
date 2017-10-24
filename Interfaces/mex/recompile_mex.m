f4acbf8463f48714a56b518d990efe03fd8101d8
Padraic Calpin <padraic.calpin93@gmail.com>
2017-10-24 22:47:35 +0100

    Changes to compilaton: This isn't portable as it stands, can we get there using a superbuild?

--- Interfaces/mex/recompile_mex.m
+++ Interfaces/mex/recompile_mex.m
@@ -1,5 +1,8 @@
-root = fullfile('/Users','padraic', 'Documents', 'PhD', 'StabilizerCPP');
+root = fullfile('/Users','padraic', 'Documents','UCL', 'PhD', 'StabilizerCPP');
+eigen_path = fullfile(root,'dependencies','eigen');
+dbitset_path = fullfile('/usr','local','include');
 inc_path = fullfile(root,'src');
 static_lib = fullfile(root,'out','libsymplectic_stabilizer.a');
-mex('-v','CXXFLAGS="$CXXFLAGS -std=c++11"',['-I',inc_path],static_lib,'mex_random_stabilizer.cpp');
-mex('-v','CXXFLAGS="$CXXFLAGS -std=c++11"',['-I',inc_path],static_lib,'mex_random_states.cpp');
+mex('-v','CXXFLAGS="$CXXFLAGS -std=c++11"',['-I',inc_path], ['-I', eigen_path], ['-I', dbitset_path], static_lib,'mex_random_stabilizer.cpp');
+% mex('-v','CXXFLAGS="$CXXFLAGS -std=c++11"',['-I',inc_path],['-I', eigen_path], ['-I', dbitset_path], static_lib,'mex_random_states.cpp');
+% TODO: Copy the mex example to fix this pointer error.
